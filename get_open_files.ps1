a5788a84d03c675690fed15ac7839e010f03759c
Patrick Moon <patrickmoon@outlook.com>
2024-02-19 11:34:29 -0800

    date fix
    Also added general notes in the file.

--- get_open_files.ps1
+++ get_open_files.ps1
@@ -1,8 +1,25 @@
+<#
+.SYNOPSIS
+    get_open_files.ps1 - Gather the open files on a server and save as a json file.
+
+.DESCRIPTION
+    Gather the open files on a server and save as a json file.
+
+.EXAMPLE
+    .\get_open_files.ps1
+
+.NOTES
+    Author: Patrick Moon
+#>
+# version = 1.01
 # Get open files
 $openFiles = Get-SmbOpenFile
 
 # Get active sessions
 $sessions = Get-SmbSession
+
+#Current DateTime
+$currentDateTime = (Get-Date).ToString("yyyy-MM-dd HH:mm")
 
 # Combine open files with session information
 $openFilesWithUsers = $openFiles | ForEach-Object {
@@ -12,7 +29,7 @@ $openFilesWithUsers = $openFiles | ForEach-Object {
         FileName = $file.Path
         UserName = $session.ClientUserName
         SessionId = $file.SessionId
-        Current_Date = (Get-Date).ToString("yyyy-MM-dd HH:mm")
+        Current_Date = $currentDateTime
     }
 }
 
@@ -20,7 +37,9 @@ $openFilesWithUsers = $openFiles | ForEach-Object {
 $jsonOutput = $openFilesWithUsers | ConvertTo-Json
 
 # Save to a JSON file
-$jsonOutput | Set-Content -Path "~\Desktop\open_files.json"
+$RandomString = -join (48..57 + 65..90 + 97..122 | Get-Random -Count 4 | ForEach-Object { [char]$_ })
+$jsonOutput | Set-Content -Path "~\Desktop\open_files_$currentDateTime_$RandomString.json"
+
 
 # Display a message
-Write-Host "Open files information saved to ~\Desktop\open_files.json"
+Write-Host "File saved to ~\Desktop\open_files.json"
